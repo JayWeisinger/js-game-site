6fff3b88d8a9b10f8384ad84291a8133955b2eb9
James W <jkm10942@gmail.com>
2020-12-25 01:58:25 -0600

    added whack a mole game
    need to fix timer isssue

--- app.js
+++ app.js
@@ -108,3 +108,52 @@ document.addEventListener('DOMContentLoaded', () => {
     createBoard()
 
 })
+
+// Whack a mole section
+
+const square = document.querySelectorAll('.square');
+const mole = document.querySelectorAll('.mole');
+const timeLeft = document.querySelector('#time-left');
+let score = document.querySelector('#score');
+
+let result = 0;
+let currentTime = timeLeft.textContent;
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole')
+    })
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+
+    //assign the id of the randomPositon to hitPosition for us to use later
+    hitPosition = randomPosition.id;
+}
+
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if(id.id === hitPosition) {
+            result = result + 1;
+            score.textContent = result;
+        }
+    })
+})
+
+function moveMole() {
+    let timerId = null;
+    timerId = setInterval(randomSquare, 900);
+}
+
+moveMole()
+
+function countDown() {
+    currentTime--;
+    timeLeft.textContent = currentTime;
+
+    if(currentTime === 0) {
+        clearInterval(timerId);
+        alert('GAME OVER! Your final score is ' + result);
+    }
+}
+
+let timerId = setInterval(countDown, 1000);
