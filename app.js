a9a3592240aed07efd44e1008b21572c9c313348
James W <jkm10942@gmail.com>
2020-12-27 14:56:36 -0600

    updating connect 4 section

--- app.js
+++ app.js
@@ -160,3 +160,41 @@ function countDown() {
 function startCountDown() {
     let timerId = setInterval(countDown, 1000);
 }
+
+// connect-4 section fix bug that if this section is gone the rest of the page works also addd another dom event listener its fine
+
+//fixed issue the issue. had to add the event listener and make sure the first part of the for loop where "i" is set is ended with a comma not a semicolon
+
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid3 div');
+    const result = document.querySelector('#result');
+    const displayCurrentPlayer = document.querySelector('#current-player');
+    let currentPlayer = 1;
+
+    for (var i = 0, len = squares.length; i < len; i++) {
+
+        (function(index) {
+        //add an onclick to each square in your grid
+            squares[i].onclick = function() {
+                //if the square below your current sqaure is taken you can go on top of it
+                if(squares[index + 7].classList.contains('taken')) {
+                    if (currentPlayer === 1) {
+                        squares[index].classList.add('taken');
+                        squares[index].classList.add('player-one');
+                        //change the player
+                        currentPlayer = 2;
+                        displayCurrentPlayer.innerHTML = currentPlayer;
+                    } else if (currentPlayer === 2) {
+                        squares[index].classList.add('taken');
+                        squares[index].classList.add('player-two');
+                        //change the player
+                        currentPlayer = 1;
+                        displayCurrentPlayer.innerHTML = currentPlayer;
+                    }
+                    // if the square below your current square is not taken, you can't go there
+                }   else alert('cant go here')  
+            }
+        })(i)
+    }
+})
+
